4e99f71f1829f0a17a7c0b95ffea347e67c0196f
Niharika1798 <niharikagogate17@gmail.com>
2020-09-21 16:23:38 +0530

    Percentage added

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 import uvicorn
 from pydantic import BaseModel
 from tensorflow import keras as K
@@ -8,6 +9,13 @@ import json
 model = K.models.load_model('model')
 
 app = FastAPI()
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins="*",
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 app.debug = True
 
 class Features(BaseModel):
